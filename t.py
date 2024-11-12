a60cb9765db0a59785560bb3b382f3fceae874cc
julian <juliansolo7189@gmail.com>
2024-11-12 12:28:46 -0500

    potencia en python

---
+++ t.py
@@ -0,0 +1,8 @@
+# La variable "potencia" almacena el resultado de elevar 23 al cuadrado
+potencia = 23 ** 2
+
+# La función print() muestra el valor de "potencia" en la consola
+print(potencia)
+
+
+
